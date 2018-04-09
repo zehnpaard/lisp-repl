f4d318dfe64e40c013b3d3379b93cb32b61e4304
zehnpaard <zehnpaard@googlemail.com>
2018-04-09 20:33:37 +0900

    Add if expression

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -4,5 +4,9 @@ import LispVal
 import Functions
 
 eval :: LispVal -> LispVal
+eval (List [Atom "if", ifForm, thenForm, elseForm]) = 
+  case eval ifForm of
+    Bool True  -> eval thenForm
+    Bool False -> eval elseForm
 eval (List (Atom funcName : args)) = apply funcName $ map eval args
 eval lispVal = lispVal
