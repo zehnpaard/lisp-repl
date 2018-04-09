e21f31b5aacc79bb3ec114acc8d8a7e72ace0b9f
zehnpaard <zehnpaard@googlemail.com>
2018-04-09 20:27:12 +0900

    Add number comparisons and logic functions

--- src/Functions.hs
+++ src/Functions.hs
@@ -12,12 +12,30 @@ primitives = [
   ("+", numBinOp (+)),
   ("-", numBinOp (-)),
   ("*", numBinOp (*)),
-  ("/", numBinOp div)
+  ("/", numBinOp div),
+  ("=", numBoolBinOp (==)),
+  ("/=", numBoolBinOp (/=)),
+  (">", numBoolBinOp (>)),
+  (">=", numBoolBinOp (>=)),
+  ("<", numBoolBinOp (<)),
+  ("<=", numBoolBinOp (<=)),
+  ("and", boolBoolBinOp (&&)),
+  ("or", boolBoolBinOp (||))
   ]
 
 numBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
 numBinOp op args = Number $ foldl1 op $ map lvToInteger args
 
+numBoolBinOp :: (Integer -> Integer -> Bool) -> [LispVal] -> LispVal
+numBoolBinOp op [x, y] = Bool $ op (lvToInteger x) (lvToInteger y)
+numBoolBinOp op _      = Bool False
+
+boolBoolBinOp op args = Bool $ foldl1 op $ map lvToBool args
+
 lvToInteger :: LispVal -> Integer
 lvToInteger (Number n) = n
 lvToInteger _          = 0
+
+lvToBool :: LispVal -> Bool
+lvToBool (Bool b) = b
+lvToBool _        = False
