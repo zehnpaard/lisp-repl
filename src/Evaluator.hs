d1042460a47b9135e66dc93d6dd3955bd60a2773
zehnpaard <zehnpaard@googlemail.com>
2018-04-11 09:28:37 +0900

    Add error handling for unevaluatable forms

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,12 +1,17 @@
 module Evaluator (eval) where
 
+import Control.Monad.Except
+
 import LispVal
+import LispError
 import Environment
 import Functions
 import Variables
 import IOThrowable
 
 eval :: EnvRef -> LispVal -> IOThrowable LispVal
+eval envRef var@(Number _) = return var
+eval envRef var@(Bool _) = return var
 eval envRef (Atom var) = getVar envRef var
 eval envRef (List [Atom "if", ifForm, thenForm, elseForm]) = do {
     ifBool <- eval envRef ifForm;
@@ -17,4 +22,4 @@ eval envRef (List [Atom "if", ifForm, thenForm, elseForm]) = do {
 eval envRef (List [Atom "set!", Atom var, form]) = eval envRef form >>= setVar envRef var 
 eval envRef (List [Atom "define", Atom var, form]) = eval envRef form >>= defineVar envRef var
 eval envRef (List (Atom funcName : args)) = mapM (eval envRef) args >>= return . apply funcName
-eval envRef lispVal = return lispVal
+eval envRef badForm = throwError $ BadFormError badForm
--- src/LispError.hs
+++ src/LispError.hs
@@ -6,9 +6,11 @@ import LispVal
 
 data LispError = ParserError ParseError
                | UnboundVar String
+               | BadFormError LispVal
 
 instance Show LispError where show = showError
 
 showError :: LispError -> String
 showError (ParserError err) = "Parser error at " ++ show err
 showError (UnboundVar var) = "Unrecognized variable: " ++ var
+showError (BadFormError form) = "Cannot evaluate form: " ++ show form
